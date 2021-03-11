c4fa1d7108a908c828e3361423bdddd5e233a109
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-03-11 22:58:51 +0700

    **

---
+++ src/test-utils/gCall.ts
@@ -0,0 +1,23 @@
+import { graphql, GraphQLSchema } from "graphql";
+import { Maybe } from "type-graphql";
+import genSchema from "../utils/genSchema";
+
+interface Options {
+	source: string;
+	variableValues?: Maybe<{
+		[key: string]: any;
+	}>;
+}
+
+let schema: GraphQLSchema;
+
+export const gCall = async ({ source, variableValues }: Options) => {
+	if (!schema) {
+		schema = await genSchema();
+	}
+	return graphql({
+		schema,
+		variableValues,
+		source,
+	});
+};
